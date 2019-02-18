7bdd45ea469f33a786b0c0db1815e1c4fef9006f
Enrique <47159293+EnriqueTovar14@users.noreply.github.com>
2019-02-17 20:27:48 -0700

    Add files via upload

---
+++ Inclass3/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inclass3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double weightE, weightM;
+                weightE = double.Parse(textBox1.Text);
+                weightM = weightE / 9.81 * 3.711;
+                textBox2.Text = weightM.ToString();
+                textBox2.Text = String.Format("{0:0.000}", weightM);
+            }
+
+            catch { 
+            MessageBox.Show("Please try again using a number");
+
+        }
+        }
+        
+    }
+}    
+
+
+
